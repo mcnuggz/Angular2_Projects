b16714a6cc0e1ffd53a59dea7df8f3423ca56255
Ryan Webb <dzanfox@gmail.com>
2017-04-03 00:10:20 -0500

    search results are displayed on page

--- ngSpotify/src/app/components/search/search.component.ts
+++ ngSpotify/src/app/components/search/search.component.ts
@@ -1,10 +1,12 @@
 import {Component} from '@angular/core';
 import {SpotifyService} from '../../services/spotify.service';
+import {Artist} from '../../../../Artist';
 
 @Component({moduleId: module.id, selector: 'search', templateUrl: `search.component.html`})
 
 export class SearchComponent {
   searchStr : string;
+  searchRes: Artist[];
   constructor(private _spotifyService : SpotifyService) {}
 
   searchMusic() {
@@ -12,7 +14,7 @@ export class SearchComponent {
       ._spotifyService
       .searchMusic(this.searchStr)
       .subscribe(res => {
-        console.log(res.artists.items);
+        this.searchRes = res.artists.items;
       });
   }
 }
