d0e1b41cb2b7067953027743afac477d115f0cb6
Ryan Webb <dzanfox@gmail.com>
2017-04-06 13:05:31 -0500

    removed quickstart

---
+++ MEANToDo/server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var todos = require('./routes/todos');
+
+//establish view engine
