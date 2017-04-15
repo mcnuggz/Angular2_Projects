3eefecf53e64b94cca3612a0688056013d8a22e0
Ryan Webb <dzanfox@gmail.com>
2017-04-15 18:35:03 -0500

    UI pages and routing set up

--- myWeather/src/pages/tabs/tabs.ts
+++ myWeather/src/pages/tabs/tabs.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { HomePage } from '../home/home';
+import { SettingsPage } from '../settings/settings';
+import { WeatherPage } from '../weather/weather';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
+  tab1Root = WeatherPage;
+  tab2Root = SettingsPage;
 
   constructor() {
 
